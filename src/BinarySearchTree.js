d849106121f9639ccff3929ced034f87b5bd00ec
Richelle Limun <rlimun@indeed.com>
2024-03-05 13:29:56 -0800

    Added comments

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -63,6 +63,7 @@ class BinarySearchTree {
     }
   }
 
+  // Left branch visited first, then current node, then right branch
   dfsInOrder(values = []) {
     if (this.left) {
       values = this.left.dfsInOrder(values);
@@ -117,37 +118,55 @@ class BinarySearchTree {
   }
 
   getHeight(currentHeight = 0) {
+    // if there is no left and right child return currentHeight, which is 0
     if (!this.left && !this.right) return currentHeight;
 
+    // if there is only left child return left height + 1
     const newHeight = currentHeight + 1;
 
+    // if there is no left child, recursively call getHeight on right child with newHeight
     if (!this.left) return this.right.getHeight(newHeight);
 
+    // if there is no right child, recursively call getHeight on left child with newHeight
     if (!this.right) return this.left.getHeight(newHeight);
 
+    // recursively call getHeight on left and right child with newHeight
     const leftHeight = this.left.getHeight(newHeight);
     const rightHeight = this.right.getHeight(newHeight);
 
+    // return maximum of left and right height
     return Math.max(leftHeight, rightHeight);
   }
 
+  /**
+   * BST - Binary Search Tree
+   * - Each node has at most two children
+   * - All nodes in left subtree have values less than node's value
+   * - All nodes in right subtree have values greater than node's value
+   * @returns 
+   */
   isBST() {
+    // perform dfs in order traversal and set values in array
     const values = this.dfsInOrder();
+    // go through the values array and check if any element is less than the previous element
     for (let i = 1; i < values.length; i++) {
       if (values[i] < values[i - 1]) {
-        return false;
+        return false; // if the element is less than previous element, return false
       }
     }
-    return true;
+    return true; // else, return true that it is a BST
   }
 
   findKthLargestValue(k) {
+    // perform in-order traversal and store values in array
     const values = this.dfsInOrder();
+    // set the kthIndex as the length of values - k
     const kthIndex = values.length - k;
 
+    // if kthIdex is greater than or equal to 0
     if (kthIndex >= 0) {
-      return values[kthIndex];
-    } else {
+      return values[kthIndex]; // return the kth largest value
+    } else {  // else k exceeds size
       console.error("k value exceeds the size of the BST.");
     }
   }
